75b56c6469d49ba4ee8675023d1509c31fced005
TnTora <128092613+TnTora@users.noreply.github.com>
2026-03-09 23:56:08 +0100

    Add volume gain support

--- YouTubeAudioPanBoost.user.js
+++ YouTubeAudioPanBoost.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         YouTube Audio Pan & Boost
-// @version      2026-03-05
+// @version      0.2
 // @description  Pan YouTube video audio left or right and boost up to x5
 // @match        https://www.youtube.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
@@ -10,17 +10,64 @@
     'use strict';
     let panNode;
     let panButton;
-    let slider;
+    let panSlider;
+    let gainNode;
+    let boostButton;
+    let boostSlider;
 
-    function toggleSlider() {
+    function togglePanSlider() {
         panNode.pan.value = 0;
-        slider.value = 0;
-        const sliderHidden = slider.style.display === 'none';
-        slider.style.display = sliderHidden ? 'block' : 'none';
+        panSlider.value = 0;
+        const sliderHidden = panSlider.style.display === 'none';
+        panSlider.style.display = sliderHidden ? 'block' : 'none';
+    }
+
+    function toggleBoostSlider() {
+        const sliderHidden = boostSlider.style.display === 'none';
+        boostSlider.style.display = sliderHidden ? 'block' : 'none';
     }
 
     function updatePanValue() {
-        panNode.pan.value = slider.value/100;
+        panNode.pan.value = panSlider.value/100;
+    }
+
+    function updateGainValue() {
+        gainNode.gain.value = boostSlider.value;
+        boostButton.innerText = `x${Number(boostSlider.value).toFixed(2)}`;
+    }
+
+    function createButton(name, inner, callback) {
+        let button;
+        button = document.createElement('button');
+        button.id = `${name}Button`;
+        button.style.background = 'none';
+        button.style.border = 'none';
+        button.style.cursor = 'pointer';
+        button.style.marginLeft = '5px';
+        //button.style.marginRight = '4px';
+        button.style.textShadow = '0px 0px 4px black';
+        button.innerText = inner;
+        button.style.color = '#fff';
+        button.style.fontWeight = 'bold';
+        button.onclick = callback;
+        return button;
+    }
+
+    function createSlider(name, min, max, step, width, callback) {
+        let slider;
+        slider = document.createElement("input");
+        slider.id = `${name}Slider`;
+        slider.type = "range";
+        slider.min = min;
+        slider.max = max;
+        slider.step = step;
+        slider.value = min;
+        slider.style.width = width;
+        slider.style.verticalAlign = "middle";
+        slider.style.margin = "auto 1px";
+        slider.style.display = "none";
+        slider.oninput = callback;
+        return slider;
     }
 
     function addButton() {
@@ -28,34 +75,19 @@
 
         console.log("addButton Pan");
 
-        //create slider
-        slider = document.createElement("input");
-        slider.type = "range";
-        slider.min = "-100";
-        slider.max = "100";
-        slider.step = "1";
-        slider.value = "0";
-        slider.style.width = "120px";
-        slider.style.verticalAlign = "middle";
-        slider.style.margin = "auto 1px";
-        slider.style.display = "none";
-        slider.oninput = updatePanValue;
+        //create sliders
+        panSlider = createSlider("pan", "-100", "100", "1", "120px", updatePanValue);
+        panSlider.value = "0";
 
+        boostSlider = createSlider("boost", "1", "5", "0.25", "90px", updateGainValue);
+        boostSlider.value = "1"
 
-        // Create Button, append it to volume controls
-        panButton = document.createElement('button');
-        panButton.id = 'panButton';
-        panButton.style.background = 'none';
-        panButton.style.border = 'none';
-        panButton.style.cursor = 'pointer';
-        panButton.style.marginLeft = '10px';
-        panButton.style.marginRight = '4px';
-        panButton.style.textShadow = '0px 0px 4px black';
-        panButton.innerText = 'Pan';
-        panButton.style.color = '#fff';
-        panButton.style.fontWeight = 'bold';
-        panButton.onclick = toggleSlider;
-        volumeArea.after(panButton, slider);
+        // Create Buttons, append elements to volume controls
+        panButton = createButton("pan", "Pan", togglePanSlider);
+        boostButton = createButton("boost", "x1.00", toggleBoostSlider);
+        volumeArea.after(boostButton, boostSlider, panButton, panSlider);
+        console.log("cliwidth: " + boostButton.clientWidth);
+        boostButton.style.width = (boostButton.clientWidth+5) + "px";
     }
 
     document.addEventListener('yt-navigate-finish', () => {
@@ -71,8 +103,10 @@
         let video = document.querySelector("video");
         let source = audioCtx.createMediaElementSource(video);
         panNode = audioCtx.createStereoPanner();
+        gainNode = audioCtx.createGain();
 
-        source.connect(panNode);
+        source.connect(gainNode);
+        gainNode.connect(panNode);
         panNode.connect(audioCtx.destination);
 
 
